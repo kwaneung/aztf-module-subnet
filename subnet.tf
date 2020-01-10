6c835c8133bd3a07dc7c868dab26478b9677f4f7
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 11:45:38 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,10 @@
 resource "azurerm_subnet" "example_subnet" {
-  count               = var.subnet_num
+  count                = var.subnet_num
   
-  name           			= var.subnet_num == 1 ? var.subnet_name: format("%s_%s_%02d", "subnet", var.subnet_name, count.index + 1)
-  resource_group_name = var.rg_name
+  name           			 = var.subnet_num == 1 ? format("%s_%s", "subnet", var.subnet_name): format("%s_%s_%02d", "subnet", var.subnet_name, count.index + 1)
+  resource_group_name  = var.rg_name
   virtual_network_name = var.vnet_name
+  var.subnet_num == 1 ? var.addr_prefix : format("%s", "subnet", var.addr_prefix)
   address_prefix       = var.addr_prefix
   
 }
