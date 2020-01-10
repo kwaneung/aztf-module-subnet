945cf8554500361b72f31c72fcbce0c4c1a54a53
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 09:52:30 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "prefix" {
+  description = "Prefix for the workspace"
+}
+
+variable subnet_name" {
+  description = "Vnet name"        
+}
+
+variable "subnet_num" {
+  description = "Number of Vnets to create"        
+}
+
+variable "addr_prefix" {
+  description = "Subnet address prefix"        
+}
+
+variable "rg_name" {
+  description = "Resource group name"        
+}
+
+variable "vnet_name" {
+ description = "Vnet name"
+}
+
+variable "tag_key" {
+  description = "Key name of tag"        
+}
+
+variable "tag_value" {
+  description = "Value name of tag"        
+}
