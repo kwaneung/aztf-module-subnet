8caef96e95a770934baf898749e6fe182a4420f4
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 15:24:15 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,37 @@
-variable "subnet_name" {
-  description = "Vnet name"        
-}
-
-variable "subnet_num" {
-  description = "Number of Vnets to create"        
-}
-
-variable "addr_prefix" {
-  description = "Subnet address prefix"        
-}
-
-variable "rg_name" {
-  description = "Resource group name"        
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  default     = ["subnet1", "subnet2", "subnet3"]
 }
 
 variable "vnet_name" {
- description = "Vnet name"
+  description = "Name of Vnet"
+}
+
+variable "resource_group_name" {
+  description = "Default resource group name that the network will be created in."
+}
+
+variable "subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  default     = ["10.0.1.0/24"]
+}
+
+variable "nsg_ids" {
+  description = "A map of subnet name to Network Security Group IDs"
+  type        = "map"
+
+  default = {
+    subnet1 = "nsgid1"
+    subnet3 = "nsgid3"
+  }
+}
+
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = "map"
+
+  default = {
+    tag1 = "test"
+    tag2 = "subnet_module"
+  }
 }
