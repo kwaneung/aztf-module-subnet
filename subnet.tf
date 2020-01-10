5a70797c8309c3ac76e4b85428f5e7b2b91d82c9
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 16:10:55 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -3,6 +3,6 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name      = var.vnet_name
   resource_group_name       = var.resource_group_name
   address_prefix            = var.subnet_prefixes[count.index]
-  network_security_group_id = "${lookup(var.nsg_ids,var.subnet_names[count.index],"")}"
+  network_security_group_id = lookup(var.nsg_ids,var.subnet_names[count.index],"")
   count                     = length(var.subnet_names)
 }
